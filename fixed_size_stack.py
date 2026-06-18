a8498238fb1d3c0d13961d74a55875a1ad9a3b28
preethi-1592006 <preethiflair159@gmail.com>
2026-06-18 22:40:59 +0530

    Add files via upload

---
+++ fixed_size_stack.py
@@ -0,0 +1,23 @@
+class FixedSizeStack:
+    def __init__(self, size):
+        self.size = size
+        self.stack = []
+
+    def push(self, item):
+        if len(self.stack) >= self.size:
+            self.stack.pop(0)
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1]
+
+    def get_all(self):
+        return self.stack
+    
